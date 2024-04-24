bda7f86567354b862504034593d72adba7e07e5c
Terry Guan <terryguan97@gmail.com>
2024-04-24 17:24:42 -0400

    hero content and more CSS

---
+++ public/js/safe-streets-roads-tompkins.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  // Annoouncement dropdown
+  $(".dropdown-button").click(function() {
+    var content = $(this).parent().next();
+    var announcement = $(this).closest('.announcement');
+    if (content.css("maxHeight") !== "0px"){
+      content.css("maxHeight", "0px");
+      $(this).children("svg").removeClass("rotate");
+      announcement.removeClass('active');
+    } else {
+      content.css("maxHeight", content.prop("scrollHeight") + "px");
+      $(this).children("svg").addClass("rotate");
+      announcement.addClass('active');
+    } 
+  });
+});
