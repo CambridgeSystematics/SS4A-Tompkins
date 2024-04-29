73cc6b3980512bb23ba6b577eac350e6c2b43a46
Terry Guan <terryguan97@gmail.com>
2024-04-29 16:45:46 -0400

    homepage sticky nav

--- public/js/safe-streets-roads-tompkins.js
+++ public/js/safe-streets-roads-tompkins.js
@@ -14,4 +14,33 @@ $(document).ready(function() {
     } 
   });
 
+  // Main nav active class
+  var heroHeader = document.querySelector('.hero');
+  var mainNav = document.querySelector('.main-nav');
+  var isSticky = false; // Flag to track whether the sticky class is currently applied
+
+  var observerAdd = new IntersectionObserver(function(entries) {
+    if (!entries[0].isIntersecting && !isSticky) {
+      mainNav.classList.add('sticky');
+      isSticky = true;
+    }
+  }, { threshold: [0.1] }); // Threshold for adding the sticky class
+
+  var observerRemove = new IntersectionObserver(function(entries) {
+    if (entries[0].isIntersecting && isSticky) {
+      mainNav.classList.remove('sticky');
+      isSticky = false;
+    }
+  }, { threshold: [0.12] }); // T´hreshold for removing the sticky class
+
+
+  observerAdd.observe(heroHeader);
+  observerRemove.observe(heroHeader);
+
+  // Scroll to top
+
+  $('.scroll-to-top').click(function(event) {
+    event.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
 });
