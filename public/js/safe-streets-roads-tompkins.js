dc42fb6928f1beafa23dc5bff195349b5ee1db72
Terry Guan <terryguan97@gmail.com>
2024-05-15 11:59:04 -0400

    responsiveness changes

--- public/js/safe-streets-roads-tompkins.js
+++ public/js/safe-streets-roads-tompkins.js
@@ -1,5 +1,54 @@
 $(document).ready(function() {
+
+  var stickyNav = document.querySelector('.sticky-nav');
+  var mqlSmall= window.matchMedia('(max-width: 768px)');
+  var mqlSticky = window.matchMedia('(max-width: 1300px)');
+  var isSticky = false; // Flag to track whether the sticky class is currently applied
   
+  // observe viewport width and toggle sticky nav if it is 768px or less
+
+  window.onload = function() {
+    if (mqlSmall.matches) {
+      stickyNav.classList.add('sticky');
+      isSticky = true;
+    }
+  };
+  function handleViewportChangeSticky(e) {
+    if (e.matches) {
+      stickyNav.classList.add('sticky');
+      isSticky = true;
+    } else {
+      if (isSticky && heroHeader.getBoundingClientRect().top >= 0) {
+        stickyNav.classList.remove('sticky');
+        isSticky = false;
+      }
+    }
+  }
+  mqlSmall.addListener(handleViewportChangeSticky);
+  handleViewportChangeSticky(mqlSmall);
+ 
+
+  // Swap to small sticky dropdown nav on viewport width 1300px or less
+
+  function handleViewportChange(e) {
+    if (e.matches) {
+      // If viewport width is 1300px or less
+      $('.sticky-nav').addClass('nav-small');
+    } else {
+      // If viewport width is more than 1300px
+      $('.sticky-nav').removeClass('nav-small');
+      $('.sticky-nav').removeClass('menu-small-active');
+    }
+  }
+  mqlSticky.addListener(handleViewportChange); // Attach listener function on state changes
+  handleViewportChange(mqlSticky); // Call listener function at run time
+
+  $('.menu-icon').click(function() {
+    $('.sticky-nav').toggleClass('menu-small-active');
+  });
+
+  if (document.body.classList.contains('homepage')) { // homepage js
+
   // Annoouncement dropdown
 
   $(".dropdown-button").click(function() {
@@ -18,13 +67,7 @@ $(document).ready(function() {
 
 
   // toggle sticky nav on scroll when it passes the hero
-
   var heroHeader = document.querySelector('.hero');
-  var stickyNav = document.querySelector('.sticky-nav');
-  var mqlSticky = window.matchMedia('(max-width: 768px)');
-  var isSticky = false; // Flag to track whether the sticky class is currently applied
-
-
   var observerAdd = new IntersectionObserver(function(entries) {
     if (!entries[0].isIntersecting && !isSticky) {
       stickyNav.classList.add('sticky');
@@ -34,7 +77,7 @@ $(document).ready(function() {
 
   var observerRemove = new IntersectionObserver(function(entries) {
     // Remove sticky if it has sticky class, hero header is in view, and viewport width over 768px
-    if (entries[0].isIntersecting && isSticky && !mqlSticky.matches){
+    if (entries[0].isIntersecting && isSticky && !mqlSmall.matches){
       stickyNav.classList.remove('sticky');
       $('.sticky-nav').removeClass('menu-small-active');
       isSticky = false;
@@ -45,53 +88,33 @@ $(document).ready(function() {
   observerAdd.observe(heroHeader);
   observerRemove.observe(heroHeader);
 
-// observe viewport width and toggle sticky nav if it is 768px or less
-
-  window.onload = function() {
-    if (mqlSticky.matches) {
-      stickyNav.classList.add('sticky');
-      isSticky = true;
-    }
-  };
-  function handleViewportChangeSticky(e) {
-    if (e.matches) {
-      stickyNav.classList.add('sticky');
-      isSticky = true;
-    } else {
-      if (isSticky && heroHeader.getBoundingClientRect().top >= 0) {
-        stickyNav.classList.remove('sticky');
-        isSticky = false;
-      }
-    }
-  }
-  mqlSticky.addListener(handleViewportChangeSticky);
-  handleViewportChangeSticky(mqlSticky);
-
   // Scroll to top button
 
   $('.scroll-to-top').click(function(event) {
     event.preventDefault();
     window.scrollTo({ top: 0, behavior: 'smooth' });
   });
- 
 
-  // Swap to small sticky dropdown nav on viewport width 1300px or less
+  } // end of homepage js
 
-  var mql = window.matchMedia('(max-width: 1300px)');
-  function handleViewportChange(e) {
-    if (e.matches) {
-      // If viewport width is 1300px or less
-      $('.sticky-nav').addClass('nav-small');
-    } else {
-      // If viewport width is more than 1300px
-      $('.sticky-nav').removeClass('nav-small');
+
+  if (document.body.classList.contains('subpage')) { // subpage js
+
+    function handleViewportChangeSubpage(e) {
+      if (e.matches) {
+        // If viewport width is 1300px or less
+        $('.sticky-nav').addClass('sticky');
+      } else {
+        // If viewport width is more than 1300px
+        $('.sticky-nav').removeClass('sticky');
+        $('.sticky-nav').removeClass('menu-small-active');
+      }
     }
-  }
-  mql.addListener(handleViewportChange); // Attach listener function on state changes
-  handleViewportChange(mql); // Call listener function at run time
+    mqlSticky.addListener(handleViewportChangeSubpage); // Attach listener function on state changes
+    handleViewportChangeSubpage(mqlSticky); // Call listener function at run time
+  
 
-  $('.menu-icon').click(function() {
-    $('.sticky-nav').toggleClass('menu-small-active');
-  });
+  }  
+
 
 });
